26d2e3590da3a7a5a68451482aa6fba45ab54e3b
Isaac James <izach.james@gmail.com>
2013-09-19 15:22:53 -0700

    Gigantic launch button text

--- Assets/Game Structure/Menu.cs
+++ Assets/Game Structure/Menu.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class Menu : MonoBehaviour {
+	
+	public GUIStyle style;
+	
 	public void OnGUI() {
-		if (GUILayout.Button("Launch")) {
+		if (GUILayout.Button("Launch", style)) {
 			LevelManager.LoadLevel(Level.Adventure);
 		}
 	}
