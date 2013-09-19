795f2b7d311e03b02eb49c5bfc06cbff82d5bd57
Isaac James <izach.james@gmail.com>
2013-09-19 15:28:53 -0700

    Switch the beam and pool back

--- Assets/Morphid/MorphidAbilityProvider.cs
+++ Assets/Morphid/MorphidAbilityProvider.cs
@@ -6,8 +6,8 @@ public class MorphidAbilityProvider : AbilityProvider {
 	protected override IEnumerable<Ability> abilitySource () {
 		StatManager s = this.GetStatManager();
 		yield return new Projectile(s){castTime = 0.2f, cooldown = 1f};
-		yield return new Beam(s);
 		yield return new Pool(s){duration = 2f, cooldown = 3f};
+		yield return new Beam(s);
 		yield return new Move(s){Speed = 30, cooldown = 2f, cost = 9};
 		yield return new Move(s){Speed = 10};
 		yield return new Move(s){Speed = 10};
